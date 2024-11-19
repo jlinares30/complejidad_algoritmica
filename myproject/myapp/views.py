9d447c9fb1da8273081b67f500b5d4e9e9f0aee5
jlinares30 <jorgelinaresrrs306@hotmail.com>
2024-11-19 17:53:33 -0500

    uploading tf-v1

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -15,7 +15,6 @@ def extraer_latencia(velocidad_str):
         velocidad_maxima = int(match.group(2))
         unidad = match.group(3)
 
-        # Convertir a kbps
         if unidad == 'Mbps':
             velocidad_minima *= 1000
             velocidad_maxima *= 1000
@@ -26,8 +25,9 @@ def extraer_latencia(velocidad_str):
 
 def crear_grafo(horario):
     G = nx.Graph()
-    proveedores = Proveedor.objects.select_related('router')
-    
+    cantidad = 93985  
+    proveedores = Proveedor.objects.select_related('router')[:cantidad]  
+
     for proveedor in proveedores:
         latencia_base = 30 if horario == 'dia' else 70
         latencia = latencia_base / (1 + proveedor.conexiones)
@@ -38,25 +38,8 @@ def crear_grafo(horario):
             nombre_router = proveedor.router.nombre
             G.add_node(nombre_router, tipo='Router')
             G.add_edge(proveedor.empresa, nombre_router, weight=latencia)
+
     return G
-
-
-def mejor_empresa_por_departamento(grafo, departamento):
-    empresas_departamento = [n for n, d in grafo.nodes(data=True) if d.get('departamento') == departamento]
-    if not empresas_departamento:
-        return None, None
-
-    mejor_empresa = None
-    menor_latencia = math.inf
-
-    for empresa in empresas_departamento:
-        latencia = sum(d['weight'] for _, _, d in grafo.edges(empresa, data=True))
-        if latencia < menor_latencia:
-            menor_latencia = latencia
-            mejor_empresa = empresa
-
-    return mejor_empresa, menor_latencia
-
 
 def grafo_a_json(G):
     nodos = [{"id": node, "tipo": G.nodes[node]['tipo']} for node in G.nodes()]
@@ -65,6 +48,53 @@ def grafo_a_json(G):
     return json.dumps({"nodos": nodos, "aristas": aristas})
 
 
+def floyd_warshall(grafo):
+    nodos = list(grafo.nodes())
+    n = len(nodos)
+    
+    dist = {u: {v: float('inf') for v in nodos} for u in nodos}
+    
+
+    for u, v, data in grafo.edges(data=True):
+        dist[u][v] = data['weight']
+        dist[v][u] = data['weight']  
+    
+    for nodo in nodos:
+        dist[nodo][nodo] = 0
+    
+    for k in nodos:
+        for i in nodos:
+            for j in nodos:
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+    
+    return dist
+
+
+def mejor_empresa_por_departamento_floyd(grafo, departamento):
+    empresas_departamento = [n for n, d in grafo.nodes(data=True) if d.get('departamento') == departamento]
+    if not empresas_departamento:
+        return None, None
+
+    distancias = floyd_warshall(grafo)
+
+    mejor_empresa = None
+    menor_latencia = float('inf')
+
+    for empresa in empresas_departamento:
+        latencia_total = sum(distancias[empresa].values())
+        if latencia_total < menor_latencia:
+            menor_latencia = latencia_total
+            mejor_empresa = empresa
+
+    return mejor_empresa, menor_latencia
+
+def grafo_por_departamento(G, departamento):
+    subgrafo = G.subgraph(
+        [n for n, d in G.nodes(data=True) if d.get('departamento') == departamento or d.get('tipo') == 'Router']
+    )
+    return subgrafo
+
 def index(request):
     departamento = request.GET.get('departamento')
     horario = request.GET.get('horario', 'dia')
@@ -72,13 +102,19 @@ def index(request):
     menor_latencia = None
 
     G = crear_grafo(horario)
-    grafo_json = grafo_a_json(G)
 
     if departamento:
-        mejor_empresa, menor_latencia = mejor_empresa_por_departamento(G, departamento)
+        mejor_empresa, menor_latencia = mejor_empresa_por_departamento_floyd(G, departamento)
+        G_departamento = grafo_por_departamento(G, departamento)
+        grafo_departamento_json = grafo_a_json(G_departamento)
+    else:
+        grafo_departamento_json = None
+
+    grafo_json = grafo_a_json(G)
 
     return render(request, 'grafo_app/index.html', {
         'grafo_json': grafo_json,
+        'grafo_departamento_json': grafo_departamento_json,
         'mejor_empresa': mejor_empresa,
         'menor_latencia': menor_latencia,
     })
